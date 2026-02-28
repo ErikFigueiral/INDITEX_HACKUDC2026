480fe27ac06bdc2c41ac2a2e12fadf6c02fe7e82
ErikFA <127146145+ErikFigueiral@users.noreply.github.com>
2026-02-28 07:06:33 +0100

    KERAS 3 versions

--- src/model_builder.py
+++ src/model_builder.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 
 IMG_SIZE = 224
 
+
 def build_embedding_model(trainable=False):
 
     base = tf.keras.applications.EfficientNetB0(
@@ -12,11 +13,17 @@ def build_embedding_model(trainable=False):
 
     base.trainable = trainable
 
-    inputs = tf.keras.Input((IMG_SIZE, IMG_SIZE, 3))
+    inputs = tf.keras.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+
     x = tf.keras.applications.efficientnet.preprocess_input(inputs)
     x = base(x, training=False)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.math.l2_normalize(x, axis=1)
+
+    # ✅ CORRECTO en Keras 3
+    x = tf.keras.layers.Lambda(
+        lambda t: tf.math.l2_normalize(t, axis=1)
+    )(x)
 
     model = tf.keras.Model(inputs, x)
+
     return model
